b86bec009504b11074320d2aa74125cde1a074a8
Github123## <shamilatps5@gmail.com>
2021-01-15 13:11:34 +0530

    env variable related changes

--- Config/connection.js
+++ Config/connection.js
@@ -2,7 +2,7 @@ const mongoClient =require('mongodb').MongoClient
 const state ={db : null}
 module.exports.connect = function(done)
 {
-  const url ="mongodb://localhost:27017"
+  const url = process.env.MONGODB_URI || "mongodb+srv://a:a@homebakescluster.nb7tu.mongodb.net/<dbname>?retryWrites=true&w=majority"//"mongodb://localhost:27017"
   const dbname = "db_HomeBakes"
   mongoClient.connect(url,{useUnifiedTopology:true},(err,data)=>{
      if(err) return done(err)
