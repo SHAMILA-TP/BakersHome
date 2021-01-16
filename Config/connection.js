b8e7defaf0e652345651e0f0776385ab63e09272
Github123## <shamilatps5@gmail.com>
2021-01-16 16:58:35 +0530

    conssection string change

--- Config/connection.js
+++ Config/connection.js
@@ -2,8 +2,7 @@ const mongoClient =require('mongodb').MongoClient
 const state ={db : null}
 module.exports.connect = function(done)
 {
-  const url = process.env.MONGODB_URI || "mongodb+srv://a:a@homebakescluster.nb7tu.mongodb.net/db_HomeBakes?retryWrites=true&w=majority"
-  //"mongodb+srv://a:a@homebakescluster.nb7tu.mongodb.net/a?retryWrites=true&w=majority"//"mongodb://localhost:27017"
+  const url =process.env.MONGODB_URI|| "mongodb+srv://a:a@homebakescluster.nb7tu.mongodb.net/a?retryWrites=true&w=majority"//"mongodb://localhost:27017"
   const dbname = "db_HomeBakes"
   mongoClient.connect(url,{useUnifiedTopology:true},(err,data)=>{
      if(err) return done(err)
