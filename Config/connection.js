b29ece481aedd732643800f7b63aa3a2314f1484
Github123## <shamilatps5@gmail.com>
2021-01-16 17:46:31 +0530

    commenting and testing

--- Config/connection.js
+++ Config/connection.js
@@ -2,9 +2,9 @@ const mongoClient =require('mongodb').MongoClient
 const state ={db : null}
 module.exports.connect = function(done)
 {
-  const url =process.env.MONGODB_URI|| "mongodb+srv://a:a@homebakescluster.nb7tu.mongodb.net/a?retryWrites=true&w=majority"//"mongodb://localhost:27017"
+  const url =process.env.MONGODB_URI|| "mongodb+srv://a:a@homebakescluster.nb7tu.mongodb.net/db_HomeBakes?retryWrites=true&w=majority"//"mongodb://localhost:27017"
   const dbname = "db_HomeBakes"
-  mongoClient.connect(url,{useUnifiedTopology:true},(err,data)=>{
+   mongoClient.connect(url,{useUnifiedTopology:true},(err,data)=>{
      if(err) return done(err)
      state.db = data.db(dbname)
     done()
