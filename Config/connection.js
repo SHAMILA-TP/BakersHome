23d6ea9fa1354243cbd618cc24f72f38a72f2019
Github123## <shamilatps5@gmail.com>
2021-01-16 13:52:33 +0530

    console removed

--- Config/connection.js
+++ Config/connection.js
@@ -5,7 +5,6 @@ module.exports.connect = function(done)
   const url = process.env.MONGODB_URI || "mongodb+srv://a:a@homebakescluster.nb7tu.mongodb.net/db_HomeBakes?retryWrites=true&w=majority"
   //"mongodb+srv://a:a@homebakescluster.nb7tu.mongodb.net/a?retryWrites=true&w=majority"//"mongodb://localhost:27017"
   const dbname = "db_HomeBakes"
-  console.log(process.env.MONGODB_URI);
   mongoClient.connect(url,{useUnifiedTopology:true},(err,data)=>{
      if(err) return done(err)
      state.db = data.db(dbname)
